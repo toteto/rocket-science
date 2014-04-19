454c536bf594c5b7ed15e7b59434e3ab8a2c4aa3
papalevski <petar.bt@gmail.com>
2014-04-19 20:04:56 +0200

    Loading pictures list
    Method for loading the list of all pictures in array.

--- VizuelnoProektGames/Flags/Game.cs
+++ VizuelnoProektGames/Flags/Game.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace VizuelnoProektGames.Flags
 {
     class Game
     {
+        public List<String> countryList { get; set; }
+
+        private void getListOfCountry()
+        {
+            DirectoryInfo MyRoot = new DirectoryInfo("../../Pictures");
+            FileInfo[] fileList = MyRoot.GetFiles();
+            foreach (FileInfo file in fileList)
+                countryList.Add(file.Name);
+        }
     }
 }
