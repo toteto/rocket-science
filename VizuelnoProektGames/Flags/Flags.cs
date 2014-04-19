54e7da7ff5f1931bf41f6c4115139c1edf4d25d4
papalevski <petar.bt@gmail.com>
2014-04-19 20:14:57 +0200

    Get Next Question
    Load the pictures in the picturebox for the next question.

--- VizuelnoProektGames/Flags/Flags.cs
+++ VizuelnoProektGames/Flags/Flags.cs
@@ -21,7 +21,34 @@ namespace VizuelnoProektGames.Flags
         private void Flags_Load(object sender, EventArgs e)
         {
             newGame = new Game();
-            
+            startGame();
+        }
+
+        private void startGame()
+        {
+            newGame.startNewGame();
+            getNextQuestion();
+            progressBar1.Value = 0;
+            timer2.Start();
+        }
+
+        private void getNextQuestion()
+        {
+            newGame.generateNextQuestion();
+
+            List<String> list = newGame.answerList;
+
+            textBox1.Text = newGame.countryList.ElementAt(newGame.currentQuestion).Replace(".png", "");
+
+            pictureBox1.Image = Image.FromFile("../../Pictures/" + list.ElementAt(0));
+            pictureBox2.Image = Image.FromFile("../../Pictures/" + list.ElementAt(1));
+            pictureBox3.Image = Image.FromFile("../../Pictures/" + list.ElementAt(2));
+            pictureBox4.Image = Image.FromFile("../../Pictures/" + list.ElementAt(3));
+
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            pictureBox8.Image = null;
         }
     }
 }
