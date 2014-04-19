795035cb5e31581d77dfdedca00cc3654877feca
papalevski <petar.bt@gmail.com>
2014-04-19 19:56:59 +0200

    New button on main form
    New button for Flags games on main form.

--- VizuelnoProektGames/Form1.Designer.cs
+++ VizuelnoProektGames/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnStartXOCeption = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStartXOCeption
@@ -41,11 +42,21 @@
             this.btnStartXOCeption.UseVisualStyleBackColor = true;
             this.btnStartXOCeption.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Flags";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 395);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnStartXOCeption);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnStartXOCeption;
+        private System.Windows.Forms.Button button1;
     }
 }
 
